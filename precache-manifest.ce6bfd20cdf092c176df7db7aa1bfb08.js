a89e3da9608c722cfcba9b7749d1825cfb714644
Brandon Odiwuor <brandon.odiwuor@gmail.com>
2019-11-15 15:04:58 +0300

    deploy

---
+++ precache-manifest.ce6bfd20cdf092c176df7db7aa1bfb08.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "5cb5110cdecfba85be23",
+    "url": "/js/chunk-vendors.fc8cc242.js"
+  },
+  {
+    "revision": "9a525ccda82b37c3b039",
+    "url": "/js/app.d6050ec6.js"
+  },
+  {
+    "revision": "6e909850476c5609c909277cde815fa5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a04b8554b9484e0f357c4ad8fdb97bc",
+    "url": "/data/medicalresults.pdf"
+  },
+  {
+    "revision": "c6ff252f292b3b06619aafdace6e6085",
+    "url": "/css/style.css"
+  },
+  {
+    "revision": "5cb5110cdecfba85be23",
+    "url": "/css/chunk-vendors.a031dfb3.css"
+  },
+  {
+    "revision": "9a525ccda82b37c3b039",
+    "url": "/css/app.153a34b3.css"
+  }
+];
